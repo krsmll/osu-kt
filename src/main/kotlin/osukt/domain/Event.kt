222bae9a35464550db2c570b126dfe06fafc8e97
krmill <krmill@ttu.ee>
2021-10-22 02:33:45 +0300

    remove unneeded json annotations

--- src/main/kotlin/osukt/domain/Event.kt
+++ src/main/kotlin/osukt/domain/Event.kt
@@ -1,17 +1,11 @@
 package osukt.domain
 
-import com.beust.klaxon.Json
 import java.time.LocalDateTime
 
 data class Event(
-    @Json(name = "display_html")
-    val displayHtml: String,
-    @Json(name = "beatmap_id")
+    val displayHtml: String, // TODO: Parse this.
     val beatmapId: Long,
-    @Json(name = "beatmapset_id")
     val beatmapSetId: Long,
-    @Json(name = "date")
     val date: LocalDateTime,
-    @Json(name = "epicfactor")
     val epicFactor: Int
 )
