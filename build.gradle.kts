530d546b02398729090dcd0371ab3cdc8532a7d4
krmill <krmill@ttu.ee>
2021-10-22 02:31:19 +0300

    deps update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val klaxonVersion = "5.5"
-val jsoupVersion = "1.13.1"
-val fuelVersion = "2.3.1"
-
 plugins {
     kotlin("jvm") version "1.4.31"
     application
@@ -18,20 +14,20 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.0")
-    implementation("commons-io:commons-io:2.6")
-    implementation("org.apache.commons:commons-lang3:3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
+    implementation("commons-io:commons-io:20030203.000550")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
 
     // Klaxon
-    implementation("com.beust:klaxon:$klaxonVersion")
+    implementation("com.beust:klaxon:5.5")
 
     // Jsoup
-    implementation("org.jsoup:jsoup:$jsoupVersion")
+    implementation("org.jsoup:jsoup:1.14.3")
 
     // Fuel
-    implementation("com.github.kittinunf.fuel:fuel:$fuelVersion")
-    implementation("com.github.kittinunf.fuel:fuel-json:$fuelVersion")
-    implementation("com.github.kittinunf.result:result:4.0.0")
+    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+    implementation("com.github.kittinunf.fuel:fuel-json:2.3.1")
+    implementation("com.github.kittinunf.result:result:5.2.0")
 }
 
 tasks.withType<KotlinCompile>() {
